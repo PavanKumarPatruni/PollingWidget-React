fbf73a1da5c1d8d86f41a5944fdb8bcd5b727eeb
Pavan Kumar Patruni <>
2019-09-06 19:40:25 +0530

    Updates

---
+++ precache-manifest.abaf1fc535c2c02329a29b4393869c07.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07802957c33aef45b7afacce228d0c26",
+    "url": "/PollingWidget-React/index.html"
+  },
+  {
+    "revision": "109faa0cafee37833235",
+    "url": "/PollingWidget-React/static/css/main.189cbd32.chunk.css"
+  },
+  {
+    "revision": "0ba75a1297c164a673af",
+    "url": "/PollingWidget-React/static/js/2.e508e6a3.chunk.js"
+  },
+  {
+    "revision": "109faa0cafee37833235",
+    "url": "/PollingWidget-React/static/js/main.289cb8ca.chunk.js"
+  },
+  {
+    "revision": "d15f676a63d87e610437",
+    "url": "/PollingWidget-React/static/js/runtime~main.81910080.js"
+  },
+  {
+    "revision": "646114132fd289fc6d1c6f824805ac64",
+    "url": "/PollingWidget-React/static/media/java.64611413.png"
+  },
+  {
+    "revision": "0cded3a3276425911d55a2552bf361bf",
+    "url": "/PollingWidget-React/static/media/js.0cded3a3.png"
+  },
+  {
+    "revision": "4ef59bd910d36ba073335b26c119f6f1",
+    "url": "/PollingWidget-React/static/media/kotlin.4ef59bd9.png"
+  },
+  {
+    "revision": "3f69ff06ff14864bf50a205bae9cc8ba",
+    "url": "/PollingWidget-React/static/media/python.3f69ff06.jpg"
+  }
+]);
